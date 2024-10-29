d7c0d47328ac576f56880bd982c7429d4902ec03
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2024-10-29 17:50:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from datetime import datetime
 from src.produce_payroll_output import *
 from src.format_date import *
 from src.preprocess_files import *
@@ -9,21 +10,23 @@ col1, col2, col3, col4 = st.columns([0.15, 0.15, 0.15, 0.55])
 
 #--------------------------------------------------------------------------------
 # START AND END DATES FOR PAY PERIOD:
+current_year = current_year = datetime.now().year
+current_month = datetime.now().month
 
 st.text("Select the start date of the pay period")
 col11, col12, col13, col14 = st.columns([0.15, 0.15, 0.15, 0.55])
-s_year = col11.number_input(label="Year", key="s_year",step = 1)
-s_month = col12.number_input(label="Month",key="s_month", step = 1)
-s_day = col13.number_input(label="Day",key="s_day",step = 1)
+s_year = col11.number_input(label="Year", key="s_year",step = 1, value = current_year)
+s_month = col12.number_input(label="Month",key="s_month", step = 1, value = current_month)
+s_day = col13.number_input(label="Day",key="s_day",step = 1, value = 1)
 
 start_date_string = " ".join([str(s_year), str(s_month), str(s_day)])
 
 st.text("Select the end date of the pay period")
 col21, col22, col23, col24 = st.columns([0.15, 0.15, 0.15, 0.55])
 
-e_year = col21.number_input(label="Year", key="e_year", step = 1)
-e_month = col22.number_input(label="Month", key="e_month", step = 1)
-e_day = col23.number_input(label="Day", key="e_day", step = 1)
+e_year = col21.number_input(label="Year", key="e_year", step = 1, value = current_year)
+e_month = col22.number_input(label="Month", key="e_month", step = 1, value = current_month)
+e_day = col23.number_input(label="Day", key="e_day", step = 1, value = 15)
 
 end_date_string = " ".join([str(e_year), str(e_month), str(e_day)])
 
